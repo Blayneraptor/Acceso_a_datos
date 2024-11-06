1cf37fe41553c8ec3be8e7ff9cd22bb71477112e
Blayneraptor <cousoworks@gmail.com>
2024-11-06 16:01:19 +0100

    Añadiendo tarea1 forma 2

--- Tarea Acceso a Ficheros/Tarea_1_2forma.java
+++ Tarea Acceso a Ficheros/Tarea_1_2forma.java
@@ -1,12 +1,18 @@
 import java.io.*;
 
-public class Tarea1 {
+public class Tarea_1_2forma {
     public static void main(String[] args) throws IOException {
         File file = new File("datos.txt");
         FileWriter fw = new FileWriter(file);
         fw.write("Nombre de la asignatura: Acceso a Datos\n");
         fw.write("Nombre del estudiante: Jose Couso\n");
         fw.close();
-        
+        FileReader fr = new FileReader(file);
+        int c;
+        while ((c = fr.read()) != -1) {
+            System.out.print((char) c);
+        }
+        fr.close();
+
     }
 }
